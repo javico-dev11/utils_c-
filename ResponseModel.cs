085d72bd2b4c8252576b598bd034310935ced81d
jvicho11@hotmail.com <j@vico..2021>
2022-11-15 16:37:47 -0500

    add ResponseModel file

---
+++ ResponseModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic_hawks.logic
+{
+    public class ResponseModel
+    {
+        public dynamic result { get; set; }
+        public bool response { get; set; }
+        public string message { get; set; }
+        public string title { get; set; }
+        public string href { get; set; }
+        public string function { get; set; }
+
+        public ResponseModel()
+        {
+            response = false;
+        }
+
+        public void SetResponse(bool r, string m = "", dynamic _result = null)
+        {
+            response = r;
+            message = m;
+            result = _result;
+
+            
+        }
+    }
+}
